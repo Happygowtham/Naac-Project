f7291869985b23925f69480aebe64ea2579c9083
Happygowtham <gowtham06787@gmail.com>
2023-04-17 13:54:58 +0530

    metrics view updated

--- Backend/naac_app/views/metricViewset.py
+++ Backend/naac_app/views/metricViewset.py
@@ -8,7 +8,7 @@ from rest_framework.views import APIView
 
 class MetricViewSet(viewsets.ModelViewSet):
     
-    queryset = Metrics.objects.all()
+    queryset = Metrics.objects.all().order_by('number')
     serializer_class = MetricSerializer
     filterset_fields = ['criteria', 'year']
 
