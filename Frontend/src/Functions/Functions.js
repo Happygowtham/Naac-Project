f87f2a643e98d7fec2fdcfb3e00b937b026de3d3
Gowtham <gowtham06787@gmail.com>
2023-08-07 18:23:46 +0530

    removed frontend

--- Frontend/src/Functions/Functions.js
+++
@@ -1,7 +0,0 @@
-export function groupBy(data, key) {
-    const result = data.reduce((acc, curr) => {
-        acc[curr[key]] = [...(acc[curr[key]] || []), curr];
-        return acc;
-    }, {});
-    return result;
-}
